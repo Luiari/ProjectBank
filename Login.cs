e9618d1227aed8a56cd413c7d06cfd978129900b
Martin Karlsson <martin.karlsson31@outlook.com>
2022-11-10 22:12:59 +0100

    Updating personal branch

--- Login.cs
+++ Login.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ProjectBank.Views;
 
 namespace ProjectBank
 {
-    class Login : User
+    class Login
     {
-        private new string Username = "Marre";
-        private new string Password = "Cykel";
-
+        private new string Username;
+        private new string Password;
         public int LoginAttempts;
         public bool IsLocked;
+
         public void Logint()
         {
+
             string text = @"
               /_\  _ __   __ _ _ __   __ _ ___   / __\ __ _ _ __ | | __
              //_\\| '_ \ / _` | '_ \ / _` / __| /__\/// _` | '_ \| |/ /
@@ -20,13 +22,21 @@ namespace ProjectBank
             \_/ \_/_| |_|\__,_|_| |_|\__,_|___/\_____/\__,_|_| |_|_|\_\";
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(text);
-
             Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Username: ");
+            Username = Console.ReadLine();
+
+            Console.WriteLine("Password: ");
+            Password = Console.ReadLine();
+            
+
             if (Username == "Marre" & Password == "Cykel")
             {
                 IsLocked = false;
                 
                 Console.WriteLine("{0," + Console.WindowWidth / 2 + "}", "+Welcome to your dashboard+");
+                DashboardView.Run();
             }
             else
                 Console.WriteLine("Wrong username or password. Please try again");
@@ -36,6 +46,9 @@ namespace ProjectBank
             {
                 Console.WriteLine("You have attempt to many times. Your account is now locked. Please contact our costomer service");
             }
+
+            
         }
+        
     }
 }
