eaf6a797dc94b027485bf9a1e230917abe49b7a6
SimonFranden <simon.franden@hotmail.com>
2022-11-10 11:11:30 +0100

    Dashboard och UI

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using ProjectBank.Views;
 namespace ProjectBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            DashboardView.Run();
         }
     }
 }
---
+++ Views/MyAccountsView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectBank.Views
+{
+    public class MyAccountsView
+    {
+        public static void Run()
+        {
+            Console.Clear();
+
+            string[] options = { "Exit" };
+            ViewUI viewUI = new ViewUI("My Accounts", options, $"Allkonto: 15 000sek\nSparkonto: 103 000sek");
+
+            int selectedOption = viewUI.Run();
+            switch (selectedOption)
+            {
+                case 0:
+                    DashboardView.Run();
+                    break;
+            }
+        }
+        
+    }
+}
