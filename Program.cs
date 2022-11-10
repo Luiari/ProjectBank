ebbb369e591facb5d585170806af498069ced167
SimonFranden <simon.franden@hotmail.com>
2022-11-10 11:12:52 +0100

    Revert "Dashboard och UI"
    This reverts commit eaf6a797dc94b027485bf9a1e230917abe49b7a6.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using ProjectBank.Views;
+
 namespace ProjectBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            DashboardView.Run();
+            Console.WriteLine("Hello World!");
         }
     }
 }
